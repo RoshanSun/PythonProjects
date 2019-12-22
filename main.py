b832933d018a0463ed62821a0a8a5cce1e954115
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2019-12-22 17:40:49 -0500

    Using Turtle module in py

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+a = 5
+print(a)
+
+roshan_turt = turtle.Turtle()
+roshan_turt.forward(100)
+roshan_turt.right(90)
+roshan_turt.forward(100)
+roshan_turt.right(90)
+roshan_turt.forward(100)
+roshan_turt.right(90)
+roshan_turt.forward(100)
+
+turtle.done()
