c96483dd0b3de9a8eb11ec54fefda573cb2f5ce9
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2019-12-22 17:51:55 -0500

    made 4 squares with same turtle

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ a = 5
 print(a)
 
 roshan_turt = turtle.Turtle()
+
+# Use turtle module to draw a square
+def makeSquare():
+    roshan_turt.forward(100)
+    roshan_turt.right(90)
+    roshan_turt.forward(100)
+    roshan_turt.right(90)
+    roshan_turt.forward(100)
+    roshan_turt.right(90)
+    roshan_turt.forward(100)
+
+makeSquare()
 roshan_turt.forward(100)
-roshan_turt.right(90)
+makeSquare()
 roshan_turt.forward(100)
-roshan_turt.right(90)
+makeSquare()
 roshan_turt.forward(100)
-roshan_turt.right(90)
+makeSquare()
 roshan_turt.forward(100)
 
 turtle.done()
